3bc78c13cbb90e464cc1fc365357451e97a68b77
Zac Willmington <zac.willmington@gmail.com>
2018-04-20 22:19:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
 
   def self.create_table
       sql =  <<-SQL
-      CREATE TABLE IF NOT EXISTS students (
+      CREATE TABLE IF NOT EXISTS students(
         id INTEGER PRIMARY KEY,
         name TEXT,
         grade INTEGER
