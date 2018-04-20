a6136233efaf51c54f956d3342316b8524e07fca
Zac Willmington <zac.willmington@gmail.com>
2018-04-20 22:17:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,7 +23,7 @@ class Student
   end
 
   def self.drop
-      drop = <<-SQL DROP TABLE students;
+      drop = <<-SQL DROP TABLE [IF EXISTST] students;
       SQL
       DB[:conn].execute(drop)
   end
