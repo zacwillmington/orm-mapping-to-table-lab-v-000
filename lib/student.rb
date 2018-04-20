2fa0a384c85326d0aa9f2514af15334940f9cfae
Zac Willmington <zac.willmington@gmail.com>
2018-04-20 22:33:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ class Student
   end
 
   def self.create(name:, grade:)
-      new_student = self.new(name:, grade:)
+      new_student = self.new(name, grade)
       new_student.save
       new_student
   end
