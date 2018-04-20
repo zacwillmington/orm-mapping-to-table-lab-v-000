4bcb35b7cf38d27337109bd3cbe6f5e573404f3a
Zac Willmington <zac.willmington@gmail.com>
2018-04-20 22:04:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,5 +2,14 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(id=nil, name, grade)
+      @id = id
+      @name = name
+      @grade = grade
+  end
+
   
 end
